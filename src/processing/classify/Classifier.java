0d6cf068423fbd9fd9c7b09359bc559cfc7234c0
Thomas de Beauchene <thomas.chauvot-de-beauchene@epitech.eu>
2015-11-15 01:03:55 +0100

    Classifier: builds dataset from directory and splits it into test set and training set

--- src/processing/classify/Classifier.java
+++ src/processing/classify/Classifier.java
@@ -4,29 +4,80 @@ import javafx.scene.layout.Pane;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.KNearest;
 import processing.pre.ImageManipulator;
 import utils.ErrorHandling;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.*;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by sal on 11/11/15.
  */
 public class Classifier
 {
-
     public Classifier(File img, Pane root)
     {
         this.start(img, root);
     }
 
+    public void train()
+    {
+        _knn = KNearest.create();
+        this.buildDataset();
+        this.splitDatasets(0.7f);
+    }
+
+    private KNearest                _knn;
+    private Map<String, List<Mat>>  _dataset = new HashMap<>();
+    private Map<String, List<Mat>>  _trainingSet = new HashMap<>();
+    private Map<String, List<Mat>>  _testSet = new HashMap<>();
+
+    private char[]                  _charClasses =
+            {
+                    'A', 'a',
+//                    'B', 'b',
+//                    'C', 'c',
+//                    'D', 'd',
+//                    'E', 'e',
+//                    'F', 'f',
+//                    'G', 'g',
+//                    'H', 'h',
+//                    'I', 'i',
+//                    'J', 'j',
+//                    'K', 'k',
+//                    'L', 'l',
+//                    'M', 'm',
+//                    'N', 'n',
+//                    'O', 'o',
+//                    'P', 'p',
+//                    'Q', 'q',
+//                    'R', 'r',
+//                    'S', 's',
+//                    'T', 't',
+//                    'U', 'u',
+//                    'V', 'v',
+//                    'X', 'x',
+//                    'Y', 'y',
+//                    'Z', 'z',
+//                    '0',
+//                    '1',
+//                    '2',
+//                    '3',
+//                    '4',
+//                    '5',
+//                    '6',
+//                    '7',
+//                    '8',
+//                    '9'
+            };
+
     private void start(File img, Pane root)
     {
         Mat m = null;
+
         try
         {
             m = Imgcodecs.imread(img.getCanonicalPath(), Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
@@ -37,21 +88,91 @@ public class Classifier
         }
         if (m != null && !m.empty())
         {
-            Imgproc.resize(m, m, new Size(150, 150));
-
-            m = ImageManipulator.applyOtsuBinarysation(m);
-
-            Rect r = findLetterBounds(m);
-
-            m = m.submat(r.y, r.y + r.height, r.x, r.x + r.width);
-
-            m = squareMat(m);
+            m = this.preProc(m);
+            this.train();
 
             ImageManipulator.showMat(root, m);
             System.out.println("elem = " + Arrays.toString(m.get(0, 0)));
         }
         else
             ErrorHandling.log(Level.WARNING, "Not an image");
+    }
+
+    private Mat preProc(Mat m)
+    {
+        m = ImageManipulator.applyOtsuBinarysation(m);
+        Rect r = this.findLetterBounds(m);
+        m = m.submat(r.y, r.y + r.height, r.x, r.x + r.width);
+        m = this.squareMat(m);
+        Imgproc.resize(m, m, new Size(24, 24));
+        return m;
+    }
+
+    private List<File> getDirContents(String path)
+    {
+        File        folder = new File(path);
+        File[]      contents = folder.listFiles();
+        List<File>  files = new ArrayList<>();
+
+        if (contents == null)
+            return files;
+        for (File f : contents)
+        {
+            if (f.isFile())
+                files.add(f);
+        }
+        return files;
+    }
+
+    /**
+     * Splits _dataset into _trainingSet and _testSet.
+     * @param ratio Size ratio of _trainingSet over _testSet.
+     */
+    private void splitDatasets(float ratio)
+    {
+        _dataset.forEach((key, value) ->
+        {
+            int i;
+
+            _trainingSet.put(key, new ArrayList<>());
+            _testSet.put(key, new ArrayList<>());
+            for (i = 0; i < value.size() * ratio; i++)
+            {
+                _trainingSet.get(key).add(value.get(i));
+            }
+            while (i < value.size())
+            {
+                _testSet.get(key).add(value.get(i));
+                ++i;
+            }
+        });
+    }
+
+    /**
+     * Iterate through dataset directories, each directory being named according to the character
+     * it refers to. Opens every image in these directories and stores them in the _dataset class attribute.
+     * Each matrix generated is stored in that map with its label as a key.
+     */
+    private void buildDataset()
+    {
+        String datasetPath = Classifier.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "resources/datasets/Hnd/";
+
+        for (char c : _charClasses)
+        {
+            List<File> samples = this.getDirContents(datasetPath + c);
+
+            if (!samples.isEmpty())
+            {
+                _dataset.put(String.valueOf(c), new ArrayList<>());
+                samples.forEach(file ->
+                {
+                    Mat smp = Imgcodecs.imread(file.getPath(), Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+
+                    if (!smp.empty())
+                        _dataset.get(String.valueOf(c)).add(this.preProc(smp));
+                });
+            }
+        }
     }
 
     private Rect findLetterBounds(Mat m)
